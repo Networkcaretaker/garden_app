5614df6e26b40da30438eea0cf035a3a7d8a9763
James Price <networkcaretaker@gmail.com>
2025-12-30 10:11:41 +0100

    scroll to top added to website app

--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Project from './pages/Project';
+import { ScrollToTop } from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
